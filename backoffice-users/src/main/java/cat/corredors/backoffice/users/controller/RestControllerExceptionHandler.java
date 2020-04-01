f4ba1b05e956be91429ba14dd5d6898e917f0a38
Jorge Torrente <jorge.torrente@t-systems.com>
2020-04-01 19:13:44 +0200

    Exception handler messages

--- backoffice-users/src/main/java/cat/corredors/backoffice/users/controller/RestControllerExceptionHandler.java
+++ backoffice-users/src/main/java/cat/corredors/backoffice/users/controller/RestControllerExceptionHandler.java
@@ -40,18 +40,21 @@ public class RestControllerExceptionHandler {
    @ResponseStatus(value = HttpStatus.FORBIDDEN)
    public ResponseError defaultErrorHandlerAccessDeniedException(final HttpServletRequest request,
            final HttpServletResponse response, final AccessDeniedException e) {
-       log.error(e.getMessage());
-       return new ResponseError(ACCESS_DENIED,
-               messageSource.getMessage(BOUsersConstants.Security.ErrorCodes.PREFIX + ACCESS_DENIED,
-                       new Object[] { e.getMessage() }, e.getMessage(), Locale.getDefault()));
+	   String err = messageSource.getMessage(BOUsersConstants.Security.ErrorCodes.PREFIX + ACCESS_DENIED,
+               new Object[] { e.getMessage() }, e.getMessage(), Locale.getDefault());
+       log.error(err, e);
+       return new ResponseError(ACCESS_DENIED, err);
    }
    
    @ExceptionHandler(ConstraintViolationException.class)
    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
    public ResponseError handleConstraintViolationException(final HttpServletRequest request,
            final HttpServletResponse response, final ConstraintViolationException e) {
-	   log.error(e.getMessage());
-	   return new ResponseError(validator.process(e.getConstraintViolations()));
+	   List<ErrorBean> errors = validator.process(e.getConstraintViolations());
+	   errors.forEach(err -> {
+		   log.error(err.getMessage());
+	   });
+	   return new ResponseError(errors);
    }
    
    @SuppressWarnings("unchecked")
@@ -59,12 +62,17 @@ public class RestControllerExceptionHandler {
    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
    public ResponseError defaultErrorHandlerSystemException(final HttpServletRequest request,
            final HttpServletResponse response, final BOUsersSystemFault e) {
-       log.error(e.getMessage());
-		if (ERR_LST == e.getCode()) {
-			return new ResponseError((List<ErrorBean>) e.getParameters()[0]);
+		if (BOUsersConstants.REST.ErrorCodes.ERR_LST == e.getCode()) {
+			List<ErrorBean> errors = (List<ErrorBean>) e.getParameters()[0];
+			errors.forEach(err -> {
+				log.error(err.getMessage());
+			});
+			return new ResponseError(errors);
 		} else {
-	       return new ResponseError(e.getCode(), messageSource
-	               .getMessage(BOUsersConstants.REST.ErrorCodes.PREFIX + e.getCode(), e.getParameters(), e.getMessage(), Locale.getDefault()));
+			String err =  messageSource
+		               .getMessage(BOUsersConstants.REST.ErrorCodes.PREFIX + e.getCode(), e.getParameters(), e.getMessage(), Locale.getDefault());
+			log.error(err);
+			return new ResponseError(e.getCode(), err);
 		}
    }
    
@@ -72,26 +80,29 @@ public class RestControllerExceptionHandler {
    @ResponseStatus(value = HttpStatus.NOT_FOUND)
    public ResponseError handleNotFoundException(final HttpServletRequest request,
            final HttpServletResponse response, final BOUserNotFoundException e) {
-	   log.error(e.getMessage());
-       return new ResponseError(e.getCode(), messageSource
-               .getMessage(BOUsersConstants.REST.ErrorCodes.PREFIX + e.getCode(), e.getParameters(), e.getMessage(), Locale.getDefault()));
+	   String err = messageSource
+               .getMessage(BOUsersConstants.REST.ErrorCodes.PREFIX + e.getCode(), e.getParameters(), e.getMessage(), Locale.getDefault());
+	   log.error(err);
+       return new ResponseError(e.getCode(), err);
    }  
 
    @ExceptionHandler(MemberPreconditionException.class)
    @ResponseStatus(value = HttpStatus.PRECONDITION_FAILED)
    public ResponseError handlePrecondtionException(final HttpServletRequest request,
            final HttpServletResponse response, final MemberPreconditionException e) {
-	   log.error(e.getMessage());
-       return new ResponseError(e.getCode(), messageSource
-               .getMessage(BOUsersConstants.REST.ErrorCodes.PREFIX + e.getCode(), e.getParameters(), e.getMessage(), Locale.getDefault()));
+	   String err = messageSource
+               .getMessage(BOUsersConstants.REST.ErrorCodes.PREFIX + e.getCode(), e.getParameters(), e.getMessage(), Locale.getDefault());
+	   log.error(err);
+       return new ResponseError(e.getCode(), err);
    }
    
    @ExceptionHandler(Exception.class)
    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
    public ResponseError defaultErrorHandlerException(final HttpServletRequest request,
            final HttpServletResponse response, final Exception e) {
-       log.error(e.getMessage(), e);
-       return new ResponseError(ERR_000, messageSource
-               .getMessage(BOUsersConstants.REST.ErrorCodes.PREFIX + ERR_000, new Object[]{ ERR_UNEXPECTED }, e.getMessage(), Locale.getDefault()));
+       String err = messageSource
+               .getMessage(BOUsersConstants.REST.ErrorCodes.PREFIX + ERR_000, new Object[]{ ERR_UNEXPECTED }, e.getMessage(), Locale.getDefault());
+       log.error(err, e);
+       return new ResponseError(ERR_000, err);
    }
 }
